c35360df2e97eaf034a2a6f7fe028d5a219ae66c
Isaac Gordezky <eye.zak.devel@gmail.com>
2012-07-19 04:27:53 -0400

    gta02-pm-usbhost: added usb_mode selection from andy-tracking:drivers/usb/host/s3c2410-ohci.c

--- arch/arm/mach-s3c24xx/gta02-pm-usbhost.c
+++ arch/arm/mach-s3c24xx/gta02-pm-usbhost.c
@@ -24,6 +24,7 @@
 #include <linux/err.h>
 #include <linux/regulator/consumer.h>
 
+#include <asm/io.h>
 #include <asm/mach-types.h>
 #include <mach/gpio.h>
 #include <mach/gta02.h>
@@ -31,13 +32,18 @@
 #include <mach/regs-gpio.h>
 #include <mach/regs-gpioj.h>
 
-static struct regulator *gta02_usbhost_regulator;
+static inline struct regulator *dev_to_regulator(struct device *dev)
+{
+	return dev_get_drvdata(dev);
+}
 
 static ssize_t usbhost_read(struct device *dev, struct device_attribute *attr,
 			char *buf)
 {
+	struct regulator * regltr = dev_to_regulator(dev);
+
 	if (!strcmp(attr->attr.name, "power_on")) {
-		if (regulator_is_enabled(gta02_usbhost_regulator))
+		if (regulator_is_enabled(regltr))
 			goto out_1;
 	}
 
@@ -48,18 +54,19 @@ out_1:
 
 static void usbhost_on_off(struct device *dev, int on)
 {
-
+	struct regulator * regltr = dev_to_regulator(dev);
+	
 	on = !!on;
 
-	if (on == regulator_is_enabled(gta02_usbhost_regulator))
+	if (on == regulator_is_enabled(regltr))
 		return;
 
 	if (!on) {
-		regulator_disable(gta02_usbhost_regulator);
+		regulator_disable(regltr);
 		return;
 	}
 
-	regulator_enable(gta02_usbhost_regulator);
+	regulator_enable(regltr);
 }
 
 static ssize_t usbhost_write(struct device *dev, struct device_attribute *attr,
@@ -107,22 +114,62 @@ static struct dev_pm_ops gta02_usbhost_pm_ops = {
 #define GTA02_USBHOST_PM_OPS NULL
 #endif /* CONFIG_PM */
 
+/* switching of USB pads */
+static ssize_t show_usb_mode(struct device *dev, struct device_attribute *attr,
+			     char *buf)
+{
+	if (__raw_readl(S3C24XX_MISCCR) & S3C2410_MISCCR_USBHOST)
+		return sprintf(buf, "host\n");
+
+	return sprintf(buf, "device\n");
+}
+
+static ssize_t set_usb_mode(struct device *dev, struct device_attribute *attr,
+			    const char *buf, size_t count)
+{
+	if (!strncmp(buf, "host", 4)) {
+		printk("s3c2410: changing usb to host\n");
+		s3c2410_modify_misccr(S3C2410_MISCCR_USBHOST,
+				      S3C2410_MISCCR_USBHOST);
+		/* FIXME:
+		 * - call machine-specific disable-pullup function i
+		 * - enable +Vbus (if hardware supports it)
+		 */
+		
+		usbhost_on_off(dev, 0);
+	} else if (!strncmp(buf, "device", 6)) {
+		printk("s3c2410: changing usb to device\n");
+		s3c2410_modify_misccr(S3C2410_MISCCR_USBHOST, 0);
+		
+		usbhost_on_off(dev, 1);
+	} else {
+		dev_err(dev, "usb_mode: unknown mode\n");
+		return -EINVAL;
+	}
+
+	return count;
+}
+
+static DEVICE_ATTR(usb_mode, S_IRUGO | S_IWUSR, show_usb_mode, set_usb_mode);
+
 static struct attribute_group gta02_usbhost_attr_group = {
 	.name	= NULL,
 	.attrs	= (struct attribute *[]) {
 		&dev_attr_power_on.attr,
+		&dev_attr_usb_mode.attr,
 		NULL
 	},
 };
 
 static int __devinit gta02_usbhost_probe(struct platform_device *pdev)
 {
+	struct regulator *regltr;
 	int ret;
 
-	gta02_usbhost_regulator = regulator_get_exclusive(&pdev->dev, "USBHOST");
+	regltr = regulator_get_exclusive(&pdev->dev, "USBHOST");
 
-	if (IS_ERR(gta02_usbhost_regulator)) {
-		ret = PTR_ERR(gta02_usbhost_regulator);
+	if (IS_ERR(regltr)) {
+		ret = PTR_ERR(regltr);
 		dev_err(&pdev->dev, "Failed to get regulator: %d\n", ret);
 		return ret;
 	}
@@ -133,15 +180,19 @@ static int __devinit gta02_usbhost_probe(struct platform_device *pdev)
 		return ret;
 	}
 
+	dev_set_drvdata(&pdev->dev, regltr);
 	return 0;
 }
 
 static int gta02_usbhost_remove(struct platform_device *pdev)
 {
+	struct regulator *regltr = dev_to_regulator(&pdev->dev);
+
 	usbhost_on_off(&pdev->dev, 0);
 
 	sysfs_remove_group(&pdev->dev.kobj, &gta02_usbhost_attr_group);
-	regulator_put(gta02_usbhost_regulator);
+	dev_set_drvdata(&pdev->dev, NULL);
+	regulator_put(regltr);
 
 	return 0;
 }
